cfc66ffa146d11c8ba0ae1753194f26ce1fcbb2e
Thanayut T <b@thanayut.in.th>
2023-02-20 13:39:59 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Home } from "./modules/Home";
 import { Start } from "./modules/Start/Start";
 import CardDisplay from "./modules/Home/CardDisplay";
 import TimePickerPage from "./modules/Start/SetNoti";
-import { FindFitness } from "./modules/FindFitness";
+// import { FindFitness } from "./modules/FindFitness";
 
 function App() {
   return (
@@ -15,10 +15,10 @@ function App() {
       <UserProvider>
         <Routes>
           <Route path="/checkin" element={<CheckIn />} />
-          <Route path="/start" element={<Start/>}/>
-          <Route path="/card" element={<CardDisplay/>}/>
-          <Route path="/setnoti" element={<TimePickerPage/>}/>
-          <Route path="/findfitness" element={<FindFitness />} />
+          <Route path="/start" element={<Start />} />
+          <Route path="/card" element={<CardDisplay />} />
+          <Route path="/setnoti" element={<TimePickerPage />} />
+          {/* <Route path="/findfitness" element={<FindFitness />} /> */}
           <Route path="*" element={<Home />} />
         </Routes>
       </UserProvider>
