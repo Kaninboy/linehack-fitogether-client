3333386e9e1a6a76921f39d849f2d2163b1bb8d8
Thanayut T <b@thanayut.in.th>
2023-02-23 21:40:12 +0700

    btn

--- src/modules/CalMemo/index.tsx
+++ src/modules/CalMemo/index.tsx
@@ -113,29 +113,32 @@ export const CalMemo = () => {
             capture="environment"
           />
         </Button>
-        {imageUrl && <img className="h-60 object-contain" src={imageUrl} />}
-        <div className="flex flex-end">
-          <button
-            className="flex justify-center items-center gap-2 w-full text-white bg-blueDark py-3 px-4 rounded-md"
-            onClick={handleSubmit}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
+        {imageUrl ? (
+          <img className="h-60 object-contain" src={imageUrl} />
+        ) : (
+          <div className="flex flex-end">
+            <button
+              className="flex justify-center items-center gap-2 w-full text-white bg-blueDark py-3 px-4 rounded-md"
+              onClick={handleSubmit}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>{" "}
-            บันทึก
-          </button>
-        </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>{" "}
+              บันทึก
+            </button>
+          </div>
+        )}
       </form>
     </div>
   );
