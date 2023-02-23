7b9e158b146192d818eeae0ed7920643efca4512
Thanayut T <b@thanayut.in.th>
2023-02-23 21:54:07 +0700

    a

--- src/modules/CalMemo/index.tsx
+++ src/modules/CalMemo/index.tsx
@@ -98,47 +98,49 @@ export const CalMemo = () => {
           }
         />
         <TextField label="บันทึก" variant="outlined" inputRef={memoRef} />
-        <Button
-          variant="contained"
-          component="label"
-          size="large"
-          disabled={!!imageUrl}
-        >
-          {imageUrl ? "อัปโหลดเรียบร้อย" : "อัปโหลดภาพประกอบ"}
-          <input
-            type="file"
-            accept="image/*"
-            hidden
-            onChange={handleUpload}
-            capture="environment"
-          />
-        </Button>
+
         {imageUrl ? (
           <img className="h-60 object-contain" src={imageUrl} />
         ) : (
-          <div className="flex flex-end">
-            <button
-              className="flex justify-center items-center gap-2 w-full text-white bg-blueDark py-3 px-4 rounded-md"
-              onClick={handleSubmit}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>{" "}
-              บันทึก
-            </button>
-          </div>
+          <Button
+            variant="contained"
+            component="label"
+            size="large"
+            disabled={!!imageUrl}
+          >
+            {imageUrl ? "อัปโหลดเรียบร้อย" : "อัปโหลดภาพประกอบ"}
+            <input
+              type="file"
+              accept="image/*"
+              hidden
+              onChange={handleUpload}
+              capture="environment"
+            />
+          </Button>
         )}
+
+        <div className="flex flex-end">
+          <button
+            className="flex justify-center items-center gap-2 w-full text-white bg-blueDark py-3 px-4 rounded-md"
+            onClick={handleSubmit}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>{" "}
+            บันทึก
+          </button>
+        </div>
       </form>
     </div>
   );
