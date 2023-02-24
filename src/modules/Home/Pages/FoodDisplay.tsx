15625d862c96801e495f6bbaf3add2ca7d5cbb16
Thanayut T <b@thanayut.in.th>
2023-02-24 13:41:44 +0700

    w

--- src/modules/Home/Pages/FoodDisplay.tsx
+++ src/modules/Home/Pages/FoodDisplay.tsx
@@ -13,7 +13,7 @@ const CardDisplay: React.FC = () => {
 
   return (
     <div
-      className="text-xs bg-cover bg-center min-h-screen"
+      className="text-xs bg-cover bg-center min-h-screen font-line"
       style={{
         backgroundImage: `url(https://storage.googleapis.com/fitogether-me/assistanceFood/Stone%20backdrops%20_%20Backdrop%20_Palermo_%20Buy%20from%20e-shop.jpg)`,
         backgroundPosition: "center",
@@ -21,14 +21,16 @@ const CardDisplay: React.FC = () => {
       }}
     >
       {item ? (
-        <Box className="font-line">
-          <div className="flex items-center">
-            <IconButton onClick={handleBack}>
-              <ArrowBack />
-            </IconButton>
-            <p>กลับไปหน้าเมนูอาหารแนะนำ</p>
+        <>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center">
+              <IconButton onClick={handleBack}>
+                <ArrowBack />
+              </IconButton>
+              <p>กลับไปหน้าเมนูอาหารแนะนำ</p>
+            </div>
           </div>
-          <Box sx={{ mt: 1, textAlign: "center" }} className="space-y-5">
+          <div className="flex flex-col gap-4 mx-5">
             <div className="flex justify-center">
               <div className="relative w-11/12 overflow-hidden">
                 <img
@@ -39,18 +41,18 @@ const CardDisplay: React.FC = () => {
                 <Typography
                   variant="h5"
                   component="h3"
-                  className="absolute -left-[54px] top-[25px] -rotate-45 text-white font-bold bg-orange-400 opacity-90 w-48"
+                  className="text-center absolute -left-[48px] top-[34px] -rotate-45 text-white font-bold bg-orange-400 opacity-90 w-48"
                 >
                   {item.calories} kcal
                 </Typography>
               </div>
             </div>
-            <div className="flex justify-center text-3xl font-bold text-blueDark">
+            <div className="flex justify-center text-3xl font-bold text-blueDark py-1">
               {item.name}
             </div>
-            <div className="m-5 py-3 w-11/12 bg-white opacity-80 rounded-lg shadow-lg">
+            <div className="py-3 bg-white opacity-80 rounded-lg shadow-lg">
               <div className="flex flex-col text-left pl-4">
-                <h2 className="text-lg">ส่วนผสม</h2>
+                <h2 className="text-lg font-bold">ส่วนผสม</h2>
                 <ol>
                   {item.ingredient.map((step: string, index: number) => (
                     <li key={index} className="mr-4">
@@ -60,9 +62,9 @@ const CardDisplay: React.FC = () => {
                 </ol>
               </div>
             </div>
-            <div className="m-5 py-3 w-11/12 bg-white opacity-80 rounded-lg shadow-lg">
+            <div className="py-3 bg-white opacity-80 rounded-lg shadow-lg">
               <div className="flex flex-col text-left pl-4">
-                <h2 className="text-lg">ขั้นตอนและวิธีการทำ</h2>
+                <h2 className="text-lg font-bold">ขั้นตอนและวิธีการทำ</h2>
                 <ol>
                   {item.howtocook.map((step: string, index: number) => (
                     <li key={index} className="mr-4">
@@ -72,15 +74,20 @@ const CardDisplay: React.FC = () => {
                 </ol>
               </div>
             </div>
-          </Box>
-          <div className="flex justify-center">
-            <Link to={item.linemanLink} target="_blank">
-              <button className="bg-lineGreen text-white text-sm text-center px-10 py-2 m-5 rounded-md">
-                สั่งอาหารผ่าน LINE MAN ตอนนี้เลย !
-              </button>
-            </Link>
+            <div className="flex justify-center">
+              <Link
+                to={item.linemanLink}
+                className="flex w-full"
+                target="_blank"
+              >
+                <button className="flex flex-wrap gap-1 items-center w-full justify-center bg-lineGreen text-white py-2 text-sm px-10 my-2 mb-6 rounded-md">
+                  สั่งอาหารผ่าน{" "}
+                  <img src="/images/lm-logo.jpeg" className="h-8" />
+                </button>
+              </Link>
+            </div>
           </div>
-        </Box>
+        </>
       ) : (
         <Box>
           <IconButton onClick={handleBack}>
